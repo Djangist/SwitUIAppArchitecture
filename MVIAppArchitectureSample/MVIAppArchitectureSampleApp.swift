4e00b91df3513e2fbd1155264ad310f60f3b78fd
Djangist <alex-pohilko@mail.ru>
2021-10-21 22:20:18 +0300

    Initial Commit

---
+++ MVIAppArchitectureSample/MVIAppArchitectureSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVIAppArchitectureSampleApp.swift
+//  MVIAppArchitectureSample
+//
+//  Created by Alex Pokhilko on 21.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct MVIAppArchitectureSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
